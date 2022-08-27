12e4658e5b5484acc9688a8dbb9716aafda313ae
KhadijahAli14 <kady.ab.2020@gmail.com>
2022-08-27 22:22:34 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import { tab } from '@testing-library/user-event/dist/tab';
 
 function App() {
+  const [tabs, setTabs] = useState([
+    { id: 1, title: 'Tab 1', content: 'tab 1 content' },
+    { id: 2, title: 'Tab 2', content: 'tab 2 content' },
+     {id:3, title: 'Tab 3', content:'tab 3 content'},
+  ]);
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+  const handleClick = (tab) => {
+    setSelectedTab(tab)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container m-5 p-5">
+        <div className="d-flex justify-content-center">
+          {tabs.map((tab) => (
+            <ul className="nav nav-tabs flex-column" key={tab.id}>
+              <li className="nav-item display-4">
+                  <button className={`nav-link ${tab.id === selectedTab.id ? 'active text-warning' : ''}`}  onClick={()=>handleClick(tab)}>{tab.title}</button>
+              </li>
+            </ul>
+          ))}
+        </div>
+        <div className="tab-content border border-warning" id="myTabContent">
+          <div className="tab-pane fade show active p-5 m-5 text-primary h3" id="home" role="tabpanel" aria-labelledby="home-tab">
+        {selectedTab.content}
+           </div>
+        </div>
+      </div>
     </div>
   );
 }
